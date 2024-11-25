17bf97054dc9b63582638bd90cdf9e6c9e9f9326
Hannah K Abril <abrilhanna6@gmail.com>
2024-11-25 13:07:28 -0500

    update detector de mmonedas

--- pruebasControl/main.py
+++ pruebasControl/main.py
@@ -1,42 +1,37 @@
-# Controla la liberación de monedas parte superior
-
-from machine import Pin, PWM
+from machine import Pin, time_pulse_us
 import time
 
-class CoinRelease:
-    def __init__(self,servo_pin):
-        self.servo=PWM(Pin(servo_pin))
-        self.servo.freq(50)
-        self.min_duty=1800
-        self.max_duty=8200
+# Configura el pin donde está conectado el sensor inductivo
+sensor_pin = Pin(15, Pin.IN)  # Asegúrate de que este pin corresponda con tu conexión
 
-    def set_servo_angle(self, angle):
-        angle=max(0,min(180,angle))
-        duty=self.min_duty + int((self.max_duty - self.min_duty) * (angle/180))
-        self.servo.duty_u16(duty)
+# Duraciones de las señales de los diferentes tipos de moneda (en microsegundos)
+# Estos valores son ejemplos y deben ser ajustados según las pruebas
+TIMER_THRESHOLD_200 = 3000  # Duración mínima de la señal para la moneda de 200
+TIMER_THRESHOLD_500 = 5000  # Duración mínima de la señal para la moneda de 500
+TIMER_THRESHOLD_1000 = 7000  # Duración mínima de la señal para la moneda de 1000
 
-    #Posiciones de la palanca
+# Función para leer el pulso del sensor y devolver la duración en microsegundos
+def read_pulse(sensor):
+    # Lee el pulso de la señal del sensor en microsegundos
+    pulse_time = time_pulse_us(sensor, 1)  # 1 significa que queremos detectar el pulso HIGH
+    return pulse_time
 
-    def open_coin_release(self):
-        self.set_servo_angle(90)
+# Función para identificar el tipo de moneda basado en el tiempo del pulso
+def detect_coin():
+    pulse_time = read_pulse(sensor_pin)
+    print(f"Duración del pulso: {pulse_time} microsegundos")
+    
+    # Determinar el tipo de moneda según la duración del pulso
+    if pulse_time > TIMER_THRESHOLD_1000:
+        print("Moneda de 1000 pesos detectada")
+    elif pulse_time > TIMER_THRESHOLD_500:
+        print("Moneda de 500 pesos detectada")
+    elif pulse_time > TIMER_THRESHOLD_200:
+        print("Moneda de 200 pesos detectada")
+    else:
+        print("Moneda no reconocida o fuera del rango esperado")
 
-    def close_coin_release(self):
-        self.set_servo_angle(180)
-
-
-#Ejemplo de uso de clase
+# Bucle principal para detectar la moneda continuamente
 while True:
-    time_release=1
-
-    servo1=CoinRelease(servo_pin=27)
-    servo2=CoinRelease(servo_pin=26)
-
-    servo1.open_coin_release()
-    time.sleep(time_release)
-    servo1.close_coin_release()
-    time.sleep(time_release)
-
-    servo2.open_coin_release()
-    time.sleep(time_release)
-    servo2.close_coin_release()
-    time.sleep(time_release)
+    detect_coin()
+    time.sleep(1)  # Espera un segundo antes de hacer otra lectura
