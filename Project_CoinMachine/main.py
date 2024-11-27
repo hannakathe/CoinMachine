babdcfc13604dbf566a541518ece28d96a20764e
Hannah K Abril <abrilhanna6@gmail.com>
2024-11-27 13:51:42 -0500

    Update pymark.conf

--- Project_CoinMachine/main.py
+++ Project_CoinMachine/main.py
@@ -1,13 +1,1 @@
-from core.game import Game
-from services.mqtt_server import MqttClient
-from services.wifi import Wifi
-from services.validate_connection import ValidateConnection
-
-wifi = Wifi()
-mqtt = MqttClient()
-validate_connection = ValidateConnection(mqtt, wifi)
-
-while True:
-    validate_connection.validate_connection()
-    game = Game(mqtt, validate_connection)
-    game.start_game()
+# main.py -- put your code here!
