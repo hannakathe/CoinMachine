569d3b851c2d3edf623aea6eaf03e382b5144c2a
Harold Camargo <hscamargo2002@gmail.com>
2024-11-26 20:59:26 -0500

    👷 Simulador de puntuacion random de la ruleta

--- Project_CoinMachine/hardware/motor_control.py
+++ Project_CoinMachine/hardware/motor_control.py
@@ -1,20 +1,26 @@
 # Controla la rotación de los motores
 import time
 from machine import Pin, PWM  # Importa los módulos necesarios
+import random
+
 
 class MotorControl:
-    def __init__(self, PIN_LEFT, PIN_RIGHT, PIN_VELOCITY):
+    def __init__(self, PIN_LEFT, PIN_RIGHT, PIN_VELOCITY, MOTOR_SPEED = 512):
         #TODO corregir como funciona el motor
         self.motor = PWM(Pin(PIN_LEFT, PIN_RIGHT, PIN_VELOCITY))
         self.current_position = 0
-    
+        self.MOTOR_SPEED = MOTOR_SPEED
+
     def start_spin(self):
         # Lógica para iniciar la rotación
-        self.motor.duty(512)  # Ajusta la velocidad
+        self.motor.duty(self.MOTOR_SPEED)  # Ajusta la velocidad
 
     def stop_spin(self):
         # Lógica para detener la rotación
         self.motor.duty(0)
+
+        #TODO Calcular posicion actual y reemplazar el random AGREGAR ENCODER
+        self.current_position = random.randint(0,4)
 
     def turn_initial_position(self):
         self.current_position = 0
